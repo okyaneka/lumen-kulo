0bb679cb547d95db21ebccc82bd71cfb5f69eb21
Oky Aneka <okyaneka@gmail.com>
2022-06-09 10:42:30 +0700

    feature: jwt auth

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,18 +2,31 @@
 
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 
-class Admin extends Model implements AuthenticatableContract, AuthorizableContract
+class Admin extends Model implements JWTSubject, AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable, HasFactory;
 
     protected $table = 'admin';
 
     protected $fillable = ['username', 'password'];
+
+    protected $hidden = ['password'];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
